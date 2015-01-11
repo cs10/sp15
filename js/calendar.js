cf9fadb5f9812ae1423a6287ae3c6e258bcbc84c
Michael Ball <cycomachead@gmail.com>
2015-01-10 22:06:51 -0800

    Work on the calendar objects some

--- js/calendar.js
+++ js/calendar.js
@@ -1,5 +1,6 @@
 // this is an idea for making the site easier to update, maybe..
 // This is major WIP and the API / format for this has yet to be finalized
+/* DATA MODELS -- OPTIONAL DATA NOTED */
 lecture = {
         title: '',
         url: 'url',
@@ -7,7 +8,6 @@ lecture = {
         classes: 'optional classes to apply'
 };
 
-/* DATA MODELS -- OPTIONAL DATA NOTED */
 reading = {
     title: 'Name',
     url: 'duh',
@@ -31,33 +31,39 @@ cs10 = window.cs10 || {};
 cs10.newLabObject = function(title, url, rq, video) {
     // FIXME -- better handle the URL via config
     // TODO  -- How does "Project Work" or no lab fit into this?
-    baseURL = '../labs/llab/html/topic.html?topic=';
+    var baseURL = '../labs/llab/html/topic.html?topic=';
     var labObj = { type: 'Lab' };
-    if (!title) {
-        labObj.title = 'No Lab'
-        return labObj;
-    }
     labObj.title = title;
+    
+    if (!title) {
+        labObj.title = 'No Lab';
+    }
     if (url) {
         labObj.url = baseURL + url;
     }
-    if (rq) {
-        labObj.RQ = rq;
-    }
-    if (video) {
-        labObj.video = video;
-    }
+    labObj.RQ = rq;
+
+    labObj.video = video;
     return labObj;
 };
 
-cs10.newReadingsObject = function(item) {
-    return {
-        type: 'Readings',
-        title: 'These are a pain in the ass'
+cs10.newReadingsObject = function(title, url, classes) {
+    var reading = {
+        type: 'Reading',
+        title: title,
+        url: url
+    };
+    
+    if (!classes) {
+        classes = 'required'; // Default option
     }
+    
+    reading.classes = 'reading ' + classes;
+    
+    return reading;
 };
 
-cs10.newLectureObject = function(title, guest) {
+cs10.newLectureObject = function(title, guest, videURL) {
     var obj = { type: 'Lecture' };
 
     if (!title) {
@@ -70,17 +76,16 @@ cs10.newLectureObject = function(title, guest) {
     var count = cs10.lectureCounter;
     var lectureURL = 'lectures/' + (count < 10 ? '0' : '' ) + count + '/';
     obj.title = title;
-    obj.url = lectureURL
-    if (guest) {
-        obj.guest = guest;
-    }
+    obj.url = lectureURL;
+    obj.guest = guest;
+    obj.video = videoURL;
     return obj;
 };
 
 cs10.newDiscussionObject = function(title, files) {
     var obj = { 'type' : 'Discussion' };
     if (!title) {
-        obj.title = 'No Discussion';
+        obj.title = 'No Class';
         return obj;
     }
     // Global Counter for lecture
@@ -90,7 +95,7 @@ cs10.newDiscussionObject = function(title, files) {
 
     if (files) {
         var count = cs10.discussionCounter;
-        var discussionURL = 'discussion/' + (count < 10 ? '0' : '' ) + count + '/';
+        var discussionURL = 'disc/' + (count < 10 ? '0' : '' ) + count + '/';
         obj.url = dsicussionURL;
     }
 
@@ -149,181 +154,181 @@ cs10.renderObject = function(obj) {
 
     html.append(content);
     return html.html();
-}
+};
 
 // ==================================================
 // ==========     SCHEDULE ITEMS           ==========
 // ==================================================
 
 // Aug 25 - 29
-cs10.week1 = [
-    readings(null),
-    lect(null),
-    lab(null),
-    lect(null),
-    lab("Welcome to <span class='snap'>snap</span>", "berkeley_bjc/intro_new/1-introduction.topic"),
-    disc('Welcome to CS10!'),
-    hw('HW0') ];
+cs10.week1 = {
+    readings: 'No Readings',
+    lectA: lect(null),
+    labA: lab(null),
+    lectB: lect(null),
+    labB: lab("Welcome to <span class='snap'>snap</span>", "berkeley_bjc/intro_new/1-introduction.topic"),
+    disc: disc('Welcome to CS10!'),
+    hw: hw('HW0') };
 
 // Sept 1 - 5
-cs10.week2 = [
-    readings(null),
-    lect(null),
-    lab('Build Your Own Blocks', 'berkeley_bjc/intro_new/2-loops-variables.topic', 1),
-    lect('Welcome and Abstraction'),
-    lab('Conditionals', 'berkeley_bjc/intro_new/3-conditionals.topic'),
-    disc('Getting Started With <span class=“snap”>snap</span>'),
-    hw('Start on HW1') ];
+cs10.week2 = {
+    readings: [ reading(null) ],
+    lectA: lect(null),
+    labA: lab('Build Your Own Blocks', 'berkeley_bjc/intro_new/2-loops-variables.topic', 1),
+    lectB: lect('Welcome and Abstraction'),
+    labB: lab('Conditionals', 'berkeley_bjc/intro_new/3-conditionals.topic'),
+    disc: disc('Getting Started With <span class=“snap”>snap</span>'),
+    hw: hw('Start on HW1') };
 
 // Sept 8 - 12
-cs10.week3 = [
-    readings(null),
-    lect('Functions'),
-    lab('Functions', 'berkeley_bjc/intro_new/4-abstraction-testing.topic', 2),
-    lect('Creativity and Abstraction'),
-    lab('Lists 1', 'berkeley_bjc/lists/lists-I.topic'),
-    disc('All about lists'),
-    hw('HW1') ];
+cs10.week3 = {
+    readings: [ reading(null) ],
+    lectA: lect('Functions'),
+    labA: lab('Functions', 'berkeley_bjc/intro_new/4-abstraction-testing.topic', 2),
+    lectB: lect('Creativity and Abstraction'),
+    labB: lab('Lists 1', 'berkeley_bjc/lists/lists-I.topic'),
+    disc: disc('All about lists'),
+    hw: hw('HW1') };
 
 // Sept 15 - 19
-cs10.week4 = [
-    readings(null),
-    lect('3D Graphics'),
-    lab('Lists 1 & HW Help', 'berkeley_bjc/lists/lists-I.topic', 3),
-    lect('Programming Paradigms'),
-    lab('Algorithms', 'berkeley_bjc/areas/algorithms.topic'),
-    disc('Lists'),
-    hw('Start on HW2') ];
+cs10.week4 = {
+    readings: [ reading(null) ],
+    lectA: lect('3D Graphics'),
+    labA: lab('Lists 1 & HW Help', 'berkeley_bjc/lists/lists-I.topic', 3),
+    lectB: lect('Programming Paradigms'),
+    labB: lab('Algorithms', 'berkeley_bjc/areas/algorithms.topic'),
+    disc: disc('Lists'),
+    hw: hw('Start on HW2') };
 
 // Sept 22 - 26
-cs10.week5 = [
-    readings(null),
-    lect(null),
-    lab(null),
-    lect('Welcome and Abstraction'),
-    lab("Welcome to <span class='snap'>snap</span>", "berkeley_bjc/intro_new/1-introduction.topic"),
-    disc('Getting Started With <span class=“snap”>snap</span>'),
-    hw('Start on HW1') ];
+cs10.week5 = {
+    readings: [ reading(null) ],
+    lectA: lect(null),
+    labA: lab(null),
+    lectB: lect('Welcome and Abstraction'),
+    labB: lab("Welcome to <span class='snap'>snap</span>", "berkeley_bjc/intro_new/1-introduction.topic"),
+    disc: disc('Getting Started With <span class=“snap”>snap</span>'),
+    hw: hw('Start on HW1') };
 
 // Sept 29 - Oct 3
-cs10.week6 = [
-    readings(null),
-    lect(null),
-    lab(null),
-    lect('Welcome and Abstraction'),
-    lab("Welcome to <span class='snap'>snap</span>", "berkeley_bjc/intro_new/1-introduction.topic"),
-    disc('Getting Started With <span class=“snap”>snap</span>'),
-    hw('Start on HW1') ];
+cs10.week6 = {
+    readings: [ reading(null) ],
+    lectA: lect(null),
+    labA: lab(null),
+    lectB: lect('Welcome and Abstraction'),
+    labB: lab("Welcome to <span class='snap'>snap</span>", "berkeley_bjc/intro_new/1-introduction.topic"),
+    disc: disc('Getting Started With <span class=“snap”>snap</span>'),
+    hw: hw('Start on HW1') };
 
 // Oct 6 - 10
-cs10.week7 = [
-    readings(null),
-    lect(null),
-    lab(null),
-    lect(null),
-    lab(null),
-    disc('Welcome to CS10!'),
-    hw('HW0') ];
+cs10.week7 = {
+    readings: [ reading(null) ],
+    lectA: lect(null),
+    labA: lab(null),
+    lectB: lect(null),
+    labB: lab(null),
+    disc: disc('Welcome to CS10!'),
+    hw: hw('HW0') };
 
 // Oct 13 - 17
-cs10.week8 = [
-    readings(null),
-    lect(null),
-    lab(null),
-    lect('Welcome and Abstraction'),
-    lab("Welcome to <span class='snap'>snap</span>", "berkeley_bjc/intro_new/1-introduction.topic"),
-    disc('Getting Started With <span class=“snap”>snap</span>'),
-    hw('Start on HW1') ];
+cs10.week8 = {
+    readings: [ reading(null) ],
+    lectA: lect(null),
+    labA: lab(null),
+    lectB: lect('Welcome and Abstraction'),
+    labB: lab("Welcome to <span class='snap'>snap</span>", "berkeley_bjc/intro_new/1-introduction.topic"),
+    disc: disc('Getting Started With <span class=“snap”>snap</span>'),
+    hw: hw('Start on HW1') };
 
 // Oct 20 - 24
-cs10.week9 = [
-    readings(null),
-    lect(null),
-    lab(null),
-    lect('Welcome and Abstraction'),
-    lab("Welcome to <span class='snap'>snap</span>", "berkeley_bjc/intro_new/1-introduction.topic"),
-    disc('Getting Started With <span class=“snap”>snap</span>'),
-    hw('Start on HW1') ];
+cs10.week9 = {
+    readings: [ reading(null) ],
+    lectA: lect(null),
+    labA: lab(null),
+    lectB: lect('Welcome and Abstraction'),
+    labB: lab("Welcome to <span class='snap'>snap</span>", "berkeley_bjc/intro_new/1-introduction.topic"),
+    disc: disc('Getting Started With <span class=“snap”>snap</span>'),
+    hw: hw('Start on HW1') };
 
 // Oct 27 - 31
-cs10.week10 = [
-    readings(null),
-    lect(null),
-    lab(null),
-    lect(null),
-    lab(null),
-    disc('Welcome to CS10!'),
-    hw('HW0') ];
+cs10.week10 = {
+    readings: [ reading(null) ],
+    lectA: lect(null),
+    labA: lab(null),
+    lectB: lect(null),
+    labB: lab(null),
+    disc: disc('Welcome to CS10!'),
+    hw: hw('HW0') };
 
 // Nov 3 - 7
-cs10.week11 = [
-    readings(null),
-    lect(null),
-    lab(null),
-    lect('Welcome and Abstraction'),
-    lab("Welcome to <span class='snap'>snap</span>", "berkeley_bjc/intro_new/1-introduction.topic"),
-    disc('Getting Started With <span class=“snap”>snap</span>'),
-    hw('Start on HW1') ];
+cs10.week11 = {
+    readings: [ reading(null) ],
+    lectA: lect(null),
+    labA: lab(null),
+    lectB: lect('Welcome and Abstraction'),
+    labB: lab("Welcome to <span class='snap'>snap</span>", "berkeley_bjc/intro_new/1-introduction.topic"),
+    disc: disc('Getting Started With <span class=“snap”>snap</span>'),
+    hw: hw('Start on HW1') };
 
 // Nov 10 - 14
-cs10.week12 = [
-    readings(null),
-    lect(null),
-    lab(null),
-    lect('Welcome and Abstraction'),
-    lab("Welcome to <span class='snap'>snap</span>", "berkeley_bjc/intro_new/1-introduction.topic"),
-    disc('Getting Started With <span class=“snap”>snap</span>'),
-    hw('Start on HW1') ];
+cs10.week12 = {
+    readings: [ reading(null) ],
+    lectA: lect(null),
+    labA: lab(null),
+    lectB: lect('Welcome and Abstraction'),
+    labB: lab("Welcome to <span class='snap'>snap</span>", "berkeley_bjc/intro_new/1-introduction.topic"),
+    disc: disc('Getting Started With <span class=“snap”>snap</span>'),
+    hw: hw('Start on HW1') };
 
 // Nov 17 - 21
-cs10.week13 = [
-    readings(null),
-    lect(null),
-    lab(null),
-    lect(null),
-    lab(null),
-    disc('Welcome to CS10!'),
-    hw('HW0') ];
+cs10.week13 = {
+    readings: [ reading(null) ],
+    lectA: lect(null),
+    labA: lab(null),
+    lectB: lect(null),
+    labB: lab(null),
+    disc: disc('Welcome to CS10!'),
+    hw: hw('HW0') };
 
 // Nov 24 - 28
-cs10.week14 = [
-    readings(null),
-    lect(null),
-    lab(null),
-    lect('Welcome and Abstraction'),
-    lab("Welcome to <span class='snap'>snap</span>", "berkeley_bjc/intro_new/1-introduction.topic"),
-    disc('Getting Started With <span class=“snap”>snap</span>'),
-    hw('Start on HW1') ];
+cs10.week14 = {
+    readings: [ reading(null) ],
+    lectA: lect(null),
+    labA: lab(null),
+    lectB: lect('Welcome and Abstraction'),
+    labB: lab("Welcome to <span class='snap'>snap</span>", "berkeley_bjc/intro_new/1-introduction.topic"),
+    disc: disc('Getting Started With <span class=“snap”>snap</span>'),
+    hw: hw('Start on HW1') };
 
 // Dec 1 - 5
-cs10.week15 = [
-    readings(null),
-    lect(null),
-    lab(null),
-    lect('Welcome and Abstraction'),
-    lab("Welcome to <span class='snap'>snap</span>", "berkeley_bjc/intro_new/1-introduction.topic"),
-    disc('Getting Started With <span class=“snap”>snap</span>'),
-    hw('Start on HW1') ];
+cs10.week15 = {
+    readings: [ reading(null) ],
+    lectA: lect(null),
+    labA: lab(null),
+    lectB: lect('Welcome and Abstraction'),
+    labB: lab("Welcome to <span class='snap'>snap</span>", "berkeley_bjc/intro_new/1-introduction.topic"),
+    disc: disc('Getting Started With <span class=“snap”>snap</span>'),
+    hw: hw('Start on HW1') };
 
 // Dec 8 - 12
-cs10.week16 = [
-    readings(null),
-    lect(null),
-    lab(null),
-    lect(null),
-    lab(null),
-    disc('Welcome to CS10!'),
-    hw('HW0') ];
+cs10.week16 = {
+    readings: [ reading(null) ],
+    lectA: lect(null),
+    labA: lab(null),
+    lectB: lect(null),
+    labB: lab(null),
+    disc: disc('Welcome to CS10!'),
+    hw: hw('HW0') };
 
 // Dec 15 - 19
-cs10.week17 = [
-    readings(null),
-    lect(null),
-    lab(null),
-    lect('Welcome and Abstraction'),
-    lab("Welcome to <span class='snap'>snap</span>", "berkeley_bjc/intro_new/1-introduction.topic"),
-    disc('Getting Started With <span class=“snap”>snap</span>'),
-    hw('Start on HW1') ];
+cs10.week17 = {
+    readings: [ reading(null) ],
+    lectA: lect(null),
+    labA: lab(null),
+    lectB: lect('Welcome and Abstraction'),
+    labB: lab("Welcome to <span class='snap'>snap</span>", "berkeley_bjc/intro_new/1-introduction.topic"),
+    disc: disc('Getting Started With <span class=“snap”>snap</span>'),
+    hw: hw('Start on HW1') };
 
 cs10.schedule = [];
 cs10.buildCal = function() {
@@ -349,12 +354,12 @@ cs10.buildCal = function() {
     var week, weekStr, isActive;
     for (i = 0; i < 17; i += 1) {
         // TODO this should be the current week of the school year.
-        isActive = i == 0 ? 'active' : '';
+        isActive = i === 0 ? 'active' : '';
         selector = 'cal-week-' + (i + 1);
         week = cs10.schedule[i].map(cs10.renderObject);
         weekStr = week.join('\n');
         calData += '<div class="tab-pane ' + isActive + '" id="' + selector +
-                 '">' + weekStr + '</div>\n'
+        '">' + weekStr + '</div>\n';
     }
     calPills.html(pillsList);
     calContent.html(calData);
