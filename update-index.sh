ba113b76d85aaa312c0c5ad217a9f8af544f4b29
Michael Ball <cycomachead@gmail.com>
2015-01-15 03:38:03 -0800

    VERY hacky temp directory listings

---
+++ update-index.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for DIR in $(find ./lecture -type d); do
+  (
+    echo -e "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
+    ls -1pa "${DIR}" | grep -v "^\./$" | grep -v ".DS_Store" | grep -v "index.html" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
+    echo -e "</pre>\n</body>\n</html>"
+  ) > "${DIR}/index.html"
+done
+
+for DIR in $(find ./discussion -type d); do
+  (
+    echo -e "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
+    ls -1pa "${DIR}" | grep -v "^\./$" | grep -v ".DS_Store" | grep -v "index.html" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
+    echo -e "</pre>\n</body>\n</html>"
+  ) > "${DIR}/index.html"
+done
