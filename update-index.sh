3a651204d520bd4bb5b94ac03c93863d95a6e988
Michael Ball <cycomachead@gmail.com>
2015-01-15 15:57:43 -0800

    Directory listing progress

--- update-index.sh
+++ update-index.sh
@@ -2,16 +2,14 @@
 
 for DIR in $(find ./lecture -type d); do
   (
-    echo -e "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
-    ls -1pa "${DIR}" | grep -v "^\./$" | grep -v ".DS_Store" | grep -v "index.html" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
-    echo -e "</pre>\n</body>\n</html>"
+    echo -e "---\ntitle: Directory Listing "
+    echo -e " \nlayout: directory\n---\n\n"
   ) > "${DIR}/index.html"
 done
 
 for DIR in $(find ./discussion -type d); do
   (
-    echo -e "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
-    ls -1pa "${DIR}" | grep -v "^\./$" | grep -v ".DS_Store" | grep -v "index.html" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
-    echo -e "</pre>\n</body>\n</html>"
+    echo -e "---\ntitle: Directory Listing "
+    echo -e " \nlayout: directory\n---\n\n"
   ) > "${DIR}/index.html"
 done
