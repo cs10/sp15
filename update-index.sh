fbbefeb825fdd6962d8768662dee17c775852c0c
Michael Ball <cycomachead@gmail.com>
2015-01-21 03:31:09 -0800

    add index files

--- update-index.sh
+++ update-index.sh
@@ -3,10 +3,11 @@
 find ./lecture -name "*@*" -type f -print0 |
 while read -d '' file
 do
+    echo $file
     (
       echo -e "---\ntitle: Directory Listing "
       echo -e " \nlayout: directory\n---\n\n"
-    ) > "${DIR}/index.html"
+    ) > "${file}/index.html"
 done
 
 # for DIR in $(find ./lecture -type d); do
