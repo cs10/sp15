6336e72ac0c5486b23c4b4e5eb0eabdd6238077f
Michael Ball <cycomachead@gmail.com>
2015-01-21 03:30:07 -0800

    update script?

--- update-index.sh
+++ update-index.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-for DIR in $(find ./lecture -type d); do
-  (
-    echo -e "---\ntitle: Directory Listing "
-    echo -e " \nlayout: directory\n---\n\n"
-  ) > "${DIR}/index.html"
+find ./lecture -name "*@*" -type f -print0 |
+while read -d '' file
+do
+    (
+      echo -e "---\ntitle: Directory Listing "
+      echo -e " \nlayout: directory\n---\n\n"
+    ) > "${DIR}/index.html"
 done
+
+# for DIR in $(find ./lecture -type d); do
+#
+# done
 
 for DIR in $(find ./discussion -type d); do
   (
